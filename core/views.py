3defca562579692e399e452114df1dfddc436eb6
Rennan Francisco Messias de Lima <mrennan.lima@gmail.com>
2019-01-09 17:48:38 -0500

    finalizado o crud de gêneros #12. closes #12.

--- core/views.py
+++ core/views.py
@@ -1,7 +1,42 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
-from .models import Genre
+from django.views import generic
+from .models import Genero
+from .forms import *
 # Create your views here.
 
-class GenreDetailView(DetailView):
-    model = Genre
+
+class IndexView(generic.TemplateView):
+    template_name = "core/index.html"
+
+
+class GeneroCriar(generic.CreateView):
+    model = Genero
+    form_class = GeneroForm
+    template_name = 'core/genero/novo.html'
+
+class GeneroEditar(generic.UpdateView):
+    model = Genero
+    form_class = GeneroForm
+    template_name = 'core/genero/editar.html'
+
+class GeneroListar(generic.ListView):
+    model = Genero
+    paginate_by = 10
+    template_name = 'core/genero/lista.html'    
+
+    def get_queryset(self):
+        nome = self.request.GET.get('nome', '')
+        return self.model.objects.filter(nome__icontains = nome)
+    
+    # def get_context_data(self, **kwargs):
+    #     context = super(MinhaListView, self).get_context_data(**kwargs)
+    #     return context
+
+class GeneroDetalhe(generic.DetailView):
+    model = Genero
+    template_name = 'core/genero/detalhe.html'
+
+
+class GeneroDeletar(generic.DeleteView):
+    model = Genero
+    template_name = "core/genero/deletar.html"
