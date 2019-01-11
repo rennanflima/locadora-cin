10aeeac202a317df71f4e862820956cb81de03e0
Rennan Francisco Messias de Lima <mrennan.lima@gmail.com>
2019-01-11 12:44:00 -0500

    add test para o model Filme

--- core/tests/test_models.py
+++ core/tests/test_models.py
@@ -14,3 +14,13 @@ class TestGenero(TestCase):
     def test_get_absolute_url(self):
         genero = mixer.blend('core.Genero', nome='Drama')
         self.assertEqual(genero.get_absolute_url(), '/admin/genero/{}/detalhe'.format(genero.pk))
+
+class TestFilme(TestCase):
+
+    def test_str(self):
+        filme = mixer.blend('core.Filme', titulo='Velocidade Máxima', titulo_original='Speed')
+        self.assertEqual(str(filme), "%s (%s)" % ('Velocidade Máxima', 'Speed'))
+
+    def test_get_absolute_url(self):
+        filme = mixer.blend('core.Filme', titulo='A Rocha', titulo_original='The Rock')
+        self.assertEqual(filme.get_absolute_url(), '/admin/filme/{}/detalhe'.format(filme.pk))
