3c3f24d4a87b020ed3647c34ea7b0ceb3d031056
woshington <wvs2@cin.ufpe.br>
2019-01-10 21:33:12 -0300

    criando diretorio media

--- locadora/settings.py
+++ locadora/settings.py
@@ -120,11 +120,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'core/static'),
-    os.path.join(BASE_DIR, 'media'),
+    os.path.join(BASE_DIR, 'core/media'),
 ]
 
 BOOTSTRAP4 = {
