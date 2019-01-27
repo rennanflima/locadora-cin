2b0a70c790a8aaa4c48581b7570ee74c4f12e74f
Rennan Francisco Messias de Lima <mrennan.lima@gmail.com>
2019-01-27 11:40:33 -0500

    fix teste behave cadastro_genero

--- features/cadastro_genero.feature
+++ features/cadastro_genero.feature
@@ -3,9 +3,9 @@ Feature: Cadastro de Generos
 	Scenario: acesso a cadastro de generos
 		Given estou na pagina principal
 		When clicar em cadastro de generos
-		Then exibira a tela para preencher os dados
+		Then exibira a tela para preencher os dados do cadastro do genero
 
 	Scenario: cadastrar genero
 		Given a tela de cadastro de generos
-		When preencho todos os dados
-		Then exibira a mensagem "Cadastro efetuado com sucesso"
+		When preencho todos os dados de genero
+		Then exibira a mensagem "Genero adicionado com sucesso"
--- features/steps/cadastro_genero.py
+++ features/steps/cadastro_genero.py
@@ -1,18 +1,18 @@
 from behave import *
 # from core.models import Genero
 
-@given(u'a tela de cadastro de clientes')
+@given(u'a tela de cadastro de generos')
 def step_impl(context):
 	context.browser.get('http://localhost:8000/admin/genero/novo')
 
-@when(u'preencho todos os dados')
+@when(u'preencho todos os dados de genero')
 def step_impl(context):
 	form = get_element(context.browser, tag='form')
 	get_element(form, name="nome").send_keys('Terror')
 	form.submit()
 	assert Genero.objects.filter(name="Terror").exists() is not False
 
-@then(u'exibira a mensagem "Cadastro efetuado com sucesso"')
+@then(u'exibira a mensagem "Genero adicionado com sucesso"')
 def step_impl(context):
 	assert context.failed is False
 
@@ -26,7 +26,7 @@ def step_impl(context):
 #    raise NotImplementedError(u'STEP: When clicar em cadastro de usuario')
 	assert True is not False
 
-@then(u'exibira a tela para preencher os dados do cadastro de cliente')
+@then(u'exibira a tela para preencher os dados do cadastro do genero')
 def step_impl(context):
  #   raise NotImplementedError(u'STEP: Then exibira a tela para preencher os dados')
 	assert context.failed is False
