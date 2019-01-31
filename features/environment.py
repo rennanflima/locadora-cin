f90e87bffc751c28aeaa6b44ce8107e93c843d48
Woshington Valdeci <woshingtonvaldeci@gmail.com>
2019-01-31 18:07:07 -0300

    Passando no travis

--- features/environment.py
+++ features/environment.py
@@ -5,12 +5,26 @@ import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'locadora.settings.behave')
 
 django.setup()
-# from core.models import Genero
 
 def before_all(context):
-    #
-    # context.models = model
-    context.browser = webdriver.Firefox()
+    # context.browser = webdriver.Chrome("/usr/local/share/chromedriver")
+    context.browser = webdriver.Chrome()
 
 def after_all(context):
     context.browser.quit()
+
+# def before_all(context):
+#     desired_caps = {
+#         'platform': 'LINUX',
+#         'browserName': 'chrome',
+#         'name': 'teste-comportamento',
+#         'client_key': '331d36f347d47c98951b34a15b6cd310',
+#         'client_secret': '040b3f266b76496698d912fa4df587ca'
+#     }
+#     context.browser = webdriver.Remote(
+#       command_executor='http://locahost:8000/wd/hub',
+#       desired_capabilities=desired_caps
+#     )
+#
+# def after_all(context):
+#     context.browser.quit()
--- features/steps/genero_step.py
+++ features/steps/genero_step.py
@@ -2,6 +2,9 @@ from nose.tools import assert_equal, assert_true
 from behave import Given, When, Then
 from core.models import Genero
 from datetime import datetime
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
 
 # Cadastro de generos
 @given('Eu navego até a página de cadastro de gênero')
@@ -10,11 +13,13 @@ def step_impl(context):
 
 @when('Eu preencho o nome do gênero')
 def step_impl(context):
-    context.browser.find_element_by_id("id_nome").send_keys("Outro{}".format(datetime.now()))
+    elem = context.browser.find_element(By.ID, "id_nome")
+    nome_genero = "Outro".format(datetime.now())
+    elem.send_keys(nome_genero)
 
 @then('Eu clico no botão salvar')
 def step_impl(context):
-    context.browser.find_element_by_id("id_submit").click()
+    context.browser.find_element(By.ID, "id_submit").click()
 
 @then('Eu sou redirecionado para a tela de detalhes')
 def step_impl(context):
