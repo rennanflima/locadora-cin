b1fbf8100784c0442b33ab04f2ae118e15d680fd
Woshington Valdeci <woshingtonvaldeci@gmail.com>
2019-02-01 10:31:49 -0300

    removendo keys do step

--- features/environment.py
+++ features/environment.py
@@ -12,6 +12,8 @@ def before_all(context):
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
     context.browser = webdriver.Chrome("/usr/local/share/chromedriver", chrome_options=chrome_options)
+    context.browser.maximize_window()
+    context.browser.implicitly_wait(20)
 
 def after_all(context):
     context.browser.quit()
