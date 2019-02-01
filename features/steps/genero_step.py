30bebd206f6bfd81e4e584cb3b83d2c0df948722
Woshington Valdeci <woshingtonvaldeci@gmail.com>
2019-02-01 10:20:38 -0300

    removendo keys do step

--- features/steps/genero_step.py
+++ features/steps/genero_step.py
@@ -3,7 +3,6 @@ from behave import Given, When, Then
 from core.models import Genero
 from datetime import datetime
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 
 
 # Cadastro de generos
