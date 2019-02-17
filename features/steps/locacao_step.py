973e2d79c1b58f54e8bc5b6be31e8752bf4074b6
woshington <wvs2@cin.ufpe.br>
2019-02-17 17:21:17 -0300

    Colocando time

--- features/steps/locacao_step.py
+++ features/steps/locacao_step.py
@@ -1,5 +1,6 @@
 from behave import *
 from selenium.webdriver.support.ui import Select
+import time
 
 @given(u'que estou na página de listagem de locações "{page}"')
 def step_impl(context, page):
@@ -27,18 +28,22 @@ def step_impl(context):
     form = context.browser.find_element_by_xpath("//form[@id='form-item-add']")
     select = Select(form.find_element_by_id('id_item'))
     select.select_by_index(1)
+    time.sleep( 5 )
 
 @when(u'clicar em Adicionar Item')
 def step_impl(context):
     context.browser.find_element_by_id("add-item-modal").click()
+    context.browser.find_element_by_class_name("alert-success")
 
 @then(u'clicar no botão Próximo')
 def step_impl(context):
-    context.browser.find_element_by_class_name('form').submit()
+    context.browser.find_element_by_xpath("//form[@id='form-avancar-locacao']").submit()
+    # context.browser.find_element_by_class_name('form').submit()
 
 @then(u'clicar no botão Concluir')
 def step_impl(context):
     context.browser.find_element_by_xpath("//form[@id='form-concluir-locacao']").submit()
+    # context.browser.find_element_by_id('form-concluir-locacao').submit()
 
 @then(u'Locação concluída com sucesso.')
 def step_impl(context):
