150e6d530717748d393d5c5a650f7a18ec8fa90e
Rennan Francisco Messias de Lima <mrennan.lima@gmail.com>
2019-02-18 14:09:46 -0500

    fix bdd

--- locadora/urls.py
+++ locadora/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     # re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
-# ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# ]
